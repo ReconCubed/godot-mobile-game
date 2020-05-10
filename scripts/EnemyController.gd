bc7455211e18cea8523323cb7d854a199154ff28
recon <recongamingsam@gmail.com>
2020-05-10 23:38:26 +1000

    Enemies now die when colliding with player

--- scripts/EnemyController.gd
+++ scripts/EnemyController.gd
@@ -4,6 +4,14 @@ export (int) var speed = 200
 
 onready var diamond = get_tree().get_root().find_node("Diamond", true, false)
 var diamondPos = Vector2(1080/2, 1920/2)
-func _physics_process(delta):
+
+func _physics_process(_delta):
 	var dir = (diamondPos - global_position).normalized()
 	move_and_slide(dir * speed)
+	
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider.name == "Player":
+			self.free()
+
+
